4b7170faefd3beac6a59d356f018f3dd90bedb9d
KutnerUri <urikutner@gmail.com>
2026-03-04 17:00:29 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 const base = process.env.ASTRO_BASE ?? "/";
 
 // https://astro.build/config
 export default defineConfig({
   base,
+  site: "https://mana.nefzen.workers.dev",
+  integrations: [sitemap()],
   redirects: {
     "/metal/": "/respect/",
   },
